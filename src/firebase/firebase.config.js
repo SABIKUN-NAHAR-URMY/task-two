ac648b9f7eebf598485373db687bf8b1009733a7
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-14 16:56:53 +0600

    done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXh4bl55VzcWjEclDqnvSt7FpioCRz8DE",
+  authDomain: "task-two-dea62.firebaseapp.com",
+  projectId: "task-two-dea62",
+  storageBucket: "task-two-dea62.appspot.com",
+  messagingSenderId: "660913919481",
+  appId: "1:660913919481:web:b5ec466dfaefa9a7851928"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
